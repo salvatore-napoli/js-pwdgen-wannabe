67d23445f08fe1cc3101620ca58c17f0d6974e87
Salvatore Napoli <snapoli1998@gmail.com>
2020-12-11 15:03:03 +0100

    Versione bonus

---
+++ js/script-bonus.js
@@ -0,0 +1,22 @@
+var name = prompt('Qual è il tuo nome?');
+var surname = prompt('Qual è il tuo cognome?');
+var age = prompt('Quanti anni hai, ' + name + ' ' + surname + '?');
+var currentYear = new Date().getFullYear();
+var birthYear = currentYear - age;
+var favouriteColor = prompt('Qual è il tuo colore preferito?');
+
+console.log("I dati dell'utente sono:\nNome: " + name + ';\nCognome: ' + surname + ';\nEtà: ' + age + ';\nAnno di nascita: ' + birthYear + ';\nColore preferito: ' + favouriteColor + '.');
+
+var userData = document.getElementById('userdata');
+userData.innerText = 'Ciao,';
+userData.append('\nI tuoi dati sono:');
+userData.append('\nNome: ' + name + ';');
+userData.append('\nCognome: ' + surname + ';');
+userData.append('\nEtà: ' + age + ';');
+userData.append('\nAnno di nascita: ' + birthYear + ';');
+userData.append('\nColore preferito: ' + favouriteColor + '.');
+
+if (name === '' || surname === '' || age === '' || birthYear === '' || favouriteColor === '') {
+  alert('You cannot leave an empty field! Please repeat the procedure.');
+  userData.innerHTML = '<div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-weight: bold; font-size: 50px;">Failed</div>';
+}
